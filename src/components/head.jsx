996c4ff3d97a1da89dcddcdb52f7308c27c76d54
Natalia Shuvaeva <glukomani@gmail.com>
2023-09-07 11:40:41 +0200

    Font and background are good now

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -1,11 +1,11 @@
 export const Head = () => {
   return (
     <div className="menu">
-      <div className="menu-item">Github</div>
-      <div className="menu-item">LinkedIn</div>
+      <div className="menu-item"><a href="https://github.com/glukomania?tab=repositories">Github</a></div>
+      <div className="menu-item"><a href="https://www.linkedin.com/in/glukomania/">LinkedIn</a></div>
       <div className="menu-item">|</div>
-      <div className="menu-item">Telegram</div>
-      <div className="menu-item">Mail</div>
+      <div className="menu-item"><a href="https://t.me/glukomania">Telegram</a></div>
+      <div className="menu-item"><a href="mailto:shuvaeva@email.cz">Mail</a></div>
       <div className="menu-item">|</div>
       <div className="menu-item">CV</div>
     </div>
--- src/components/textBlock.jsx
+++ src/components/textBlock.jsx
@@ -1,10 +1,10 @@
 export const TextBlock = () => {
   return (
-    <div>
-      <div>
+    <div className="textBlock">
+      <div className="textBlock__name">
       Shuvaeva Natalia
       </div>
-      <div>
+      <div className="textBlock__text">
       Hi! I'm a frontend and app developer living in Prague, 
 and I'm currently seeking an exciting project to be a part of,
  preferably within a startup. I'm eagerly anticipating hearing from you!s
